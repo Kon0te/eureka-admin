b169a2e77ac00b6b1d23381f595a9ba9c91e0d24
Mickaël Virasone <mickaelvirasone@Mac-mini-de-Mickael.local>
2026-08-31 02:24:31 +0200

    deploy: align Premium with funnel flow

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,7 +33,7 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=a6399fb"},{}]};
+_flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06cd","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=49d53f0"},{}]};
 
 
 // Flutter's generated service worker is deprecated and previously kept an
